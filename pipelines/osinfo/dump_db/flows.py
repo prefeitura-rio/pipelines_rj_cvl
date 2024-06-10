2416e73b64c6757e46a9c05bbb4ff222b4e0a0a5
Bruno Almeida <brunodepauloalmeida@gmail.com>
2024-06-10 17:37:29 -0300

    fix: default parameter

--- pipelines/osinfo/dump_db/flows.py
+++ pipelines/osinfo/dump_db/flows.py
@@ -43,7 +43,7 @@ os_info_default_parameters = {
     "db_charset": "utf8",
     "materialize_to_datario": False,
     "biglake_table": True,
-    "dump_to_gcs": False,
+    "dump_to_gcs": True,
 }
 os_info_flow = set_default_parameters(os_info_flow, default_parameters=os_info_default_parameters)
 
