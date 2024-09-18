addbbee4cbf3ab47c7c85328d68bab544338fa31
Stella Costa <stellamaryane@gmail.com>
2024-09-18 11:00:17 -0300

    Add processorio

--- pipelines/flows.py
+++ pipelines/flows.py
@@ -4,4 +4,5 @@ Imports all flows for every project so we can register all of them.
 """
 from pipelines.exemplo import *  # noqa
 from pipelines.osinfo import *  # noqa
+from pipelines.processorio import *  # noqa
 from pipelines.templates import *  # noqa
