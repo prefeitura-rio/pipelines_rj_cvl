71ed8e11d49ac6de3d527ecdb2c2419b14b56df9
Stella Costa <stellamaryane@gmail.com>
2024-09-18 10:31:00 -0300

    Add processorio

---
+++ pipelines/processorio/dump_db/flows.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Database dumping flows for processorio..
+"""
+
+
+from copy import deepcopy
+
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+from prefeitura_rio.pipelines_templates.dump_db.flows import flow as dump_sql_flow
+from prefeitura_rio.pipelines_utils.prefect import set_default_parameters
+from prefeitura_rio.pipelines_utils.state_handlers import (
+    handler_initialize_sentry,
+    handler_inject_bd_credentials,
+)
+
+from pipelines.constants import constants
+from pipelines.processorio.dump_db.schedules import (
+    processorio_infra_daily_update_schedule,
+)
+
+rj_cvl_processorio_flow = deepcopy(dump_sql_flow)
+rj_cvl_processorio_flow.state_handlers = [
+    handler_inject_bd_credentials,
+    handler_initialize_sentry,
+]
+rj_cvl_processorio_flow.name = "CASA CIVIL: processo.rio - Ingerir tabelas de banco SQL"
+rj_cvl_processorio_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+
+rj_cvl_processorio_flow.run_config = KubernetesRun(
+    image=constants.DOCKER_IMAGE.value,
+    labels=[
+        constants.RJ_IPLANRIO_AGENT_LABEL.value,  # label do agente
+    ],
+    # cpu_limit="500m",
+    # cpu_request="500m",
+    # memory_limit="2Gi",
+    # memory_request="2Gi",
+)
+
+processorio_default_parameters = {
+    "db_database": "DW_BI_PROCESSO_RIO",
+    "db_host": "10.2.231.73",
+    "db_port": "1433",
+    "db_type": "sql_server",
+    "dataset_id": "adm_processo_interno_processorio",
+    "infisical_secret_path": "/db-processorio",
+}
+
+rj_cvl_processorio_flow = set_default_parameters(
+    rj_cvl_processorio_flow,
+    default_parameters=processorio_default_parameters,
+)
+
+rj_cvl_processorio_flow.schedule = processorio_infra_daily_update_schedule
---
+++ pipelines/processorio/dump_db/schedules.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+"""
+Schedules for the database dump pipeline.
+"""
+
+from datetime import datetime, timedelta
+
+import pytz
+from prefect.schedules import Schedule
+from prefeitura_rio.pipelines_utils.io import untuple_clocks as untuple
+from prefeitura_rio.pipelines_utils.prefect import generate_dump_db_schedules
+
+from pipelines.constants import constants
+
+#####################################
+#
+# Processorio Schedules
+#
+#####################################
+
+_processorio_infra_query = {
+    "dim_data": {
+        "biglake_table": True,
+        "materialize_after_dump": True,
+        "materialization_mode": "prod",
+        "materialize_to_datario": False,
+        "dump_to_gcs": False,
+        "dump_mode": "overwrite",
+        "execute_query": """
+            SELECT
+                IDDATA,
+                DATA,
+                DATA_INICIO_SEMANA,
+                DATA_FIM_SEMANA,
+                NUMERO_DIA_SEMANA,
+                NOME_DIA_SEMANA,
+                NUMERO_DIA_MES,
+                IDSEMANA,
+                NUMERO_SEMANA_NO_MES,
+                NUMERO_SEMANA_NO_ANO,
+                NOME_SEMANA_NO_MES,
+                NOME_SEMANA_NO_ANO,
+                IDQUINZENA,
+                NUMERO_QUINZENA,
+                NOME_QUINZENA,
+                NOME_QUINZENA_ANO,
+                IDMES,
+                NUMERO_MES,
+                NOME_MES,
+                IDTRIMESTRE,
+                NOME_TRIMESTRE,
+                IDSEMESTRE,
+                NOME_SEMESTRE,
+                IDANO,
+                NOME_ANO,
+                IDDIA_UTIL,
+                DESC_DIA_UTIL
+            FROM
+                DW_BI_PROCESSO_RIO.dbo.DIM_DATA;
+        """,  # noqa
+    },
+    "dim_setor_orgao": {
+        "biglake_table": True,
+        "materialize_after_dump": True,
+        "materialization_mode": "prod",
+        "materialize_to_datario": False,
+        "dump_to_gcs": False,
+        "dump_mode": "overwrite",
+        "execute_query": """
+            SELECT
+                IDSETOR,
+                IDORGAO,
+                SIGLA_ORGAO,
+                NOME_ORGAO,
+                SIGLA_SETOR,
+                NOME_SETOR
+            FROM
+                DW_BI_PROCESSO_RIO.dbo.DIM_SETOR_ORGAO;
+        """,  # noqa
+    },
+    "dim_tipo_evento": {
+        "biglake_table": True,
+        "materialize_after_dump": True,
+        "materialization_mode": "prod",
+        "materialize_to_datario": False,
+        "dump_to_gcs": False,
+        "dump_mode": "overwrite",
+        "execute_query": """
+            SELECT
+                IDTIPO_EVENTO,
+                NOME_EVENTO,
+                IDGRUPO_EVENTO,
+                NOME_GRUPO_EVENTO
+            FROM
+                DW_BI_PROCESSO_RIO.dbo.DIM_TIPO_EVENTO;
+        """,  # noqa
+    },
+    "dim_documento": {
+        "biglake_table": True,
+        "materialize_after_dump": True,
+        "materialization_mode": "prod",
+        "materialize_to_datario": False,
+        "dump_to_gcs": False,
+        "dump_mode": "overwrite",
+        "execute_query": """
+            SELECT
+                IDDOCUMENTO,
+                NUMERO_DOCUMENTO,
+                IDCLASSIFICACAO,
+                NOME_CLASSIFICACAO,
+                CODIGO_CLASSIFICACAO,
+                IDSUBCLASSE,
+                NOME_SUBCLASSE,
+                IDCLASSE,
+                NOME_CLASSE,
+                IDASSUNTO,
+                NOME_ASSUNTO,
+                IDFORMA_DOCUMENTO,
+                NOME_FORMA_DOCUMENTO,
+                IDTIPO_FORMA_DOCUMENTO,
+                NOME_TIPO_FORMA_DOCUMENTO
+            FROM
+                DW_BI_PROCESSO_RIO.dbo.DIM_DOCUMENTO;
+        """,  # noqa
+    },
+    "fato_tramitacao_documento": {
+        "biglake_table": True,
+        "materialize_after_dump": True,
+        "materialization_mode": "prod",
+        "materialize_to_datario": False,
+        "dump_to_gcs": False,
+        "dump_mode": "overwrite",
+        "execute_query": """
+            SELECT
+                IDACAO,
+                IDSTATUS_CICLO,
+                IDDOCUMENTO,
+                IDSETOR,
+                IDTIPO_EVENTO,
+                IDDATA_INICIO,
+                IDDATA_FIM,
+                HORA_REGISTRO_INICIO,
+                HORA_REGISTRO_FIM,
+                IDDATA_INICIO_CICLO,
+                IDDATA_FIM_CICLO,
+                DATA_HORA_CARGA,
+            FROM
+                DW_BI_PROCESSO_RIO.dbo.FATO_TRAMITACAO_DOCUMENTO;
+        """,  # noqa
+    },
+    "dim_status_ciclo": {
+        "biglake_table": True,
+        "materialize_after_dump": True,
+        "materialization_mode": "prod",
+        "materialize_to_datario": False,
+        "dump_to_gcs": False,
+        "dump_mode": "overwrite",
+        "execute_query": """
+            SELECT
+                IDSTATUS_CICLO,
+                NOME_STATUS_CICLO
+            FROM
+                DW_BI_PROCESSO_RIO.dbo.DIM_STATUS_CICLO;
+        """,  # noqa
+    },
+}
+
+processorio_infra_clocks = generate_dump_db_schedules(
+    interval=timedelta(days=1),
+    start_date=datetime(2024, 9, 18, 2, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+    labels=[
+        constants.RJ_CVL_AGENT_LABEL.value,
+    ],
+    db_database="DW_BI_PROCESSO_RIO",
+    db_host="10.2.231.73",
+    db_port="1433",
+    db_type="sql_server",
+    dataset_id="adm_processo_interno_processorio",
+    infisical_secret_path="/db-processorio",
+    table_parameters=_processorio_infra_query,
+)
+
+processorio_infra_daily_update_schedule = Schedule(clocks=untuple(processorio_infra_clocks))
